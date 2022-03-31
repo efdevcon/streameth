f514d9468dd5c690d9b1e5ecc9d8b6903cb3d387
Hans Yadav <hansy@users.noreply.github.com>
2022-03-31 02:43:19 -0400

    css

--- src/components/Player/Status/index.tsx
+++ src/components/Player/Status/index.tsx
@@ -5,7 +5,11 @@ export default function PlayerStatus() {
         <div className="player__live__dot"></div>
         <div className="player__live__description">Streaming live</div>
       </div>
-      <div>date</div>
+      <div className="player__date">
+        APR 18 -
+        <br />
+        <span>16:35 CET</span>
+      </div>
     </div>
   )
 }
