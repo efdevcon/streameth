c61cbbe6a9a17a6f84c9365a5279995426a2f925
Hans Yadav <hansy@users.noreply.github.com>
2022-04-11 00:57:31 -0400

    add some janky css to get active event on event switcher

--- src/components/Event/Switcher/index.tsx
+++ src/components/Event/Switcher/index.tsx
@@ -8,19 +8,21 @@ export default function EventSwitcher({ eventNames, activeEventName, onEventSwit
   return (
     <div className="event__switcher">
       <div className="event__switcher__title">Select event stream</div>
-      <ul className="event__switcher__events">
-        {eventNames.map(name => {
-          return (
-            <li
-              key={name}
-              onClick={() => onEventSwitch(name)}
-              className={`event__switcher__events__event ${activeEventName === name ? 'active' : ''}`}
-            >
-              {name}
-            </li>
-          )
-        })}
-      </ul>
+      <div className="event__switcher__scroll">
+        <ul className="event__switcher__events">
+          {eventNames.map(name => {
+            return (
+              <li
+                key={name}
+                onClick={() => onEventSwitch(name)}
+                className={`event__switcher__events__event ${activeEventName === name ? 'active' : ''}`}
+              >
+                {name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
