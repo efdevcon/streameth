283d50a46d1a7e2261c41fa424121b61479eed08
Hans Yadav <hansy@users.noreply.github.com>
2022-04-12 19:52:10 -0400

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,13 @@
 import { GetStaticProps } from 'next'
 import { GetEvents } from 'services/event'
-import { Event, Stream } from 'types'
+import { Event } from 'types'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { getStreams } from '../services/stream'
+
 interface Props {
   events: Event[]
 }
 
 export default function Home(props: Props) {
-  const [streams, setStreams] = useState<Stream[]>([])
-
-  useEffect(() => {
-    const fetchStreams = async () => {
-      const fetchedStreams = await getStreams()
-
-      setStreams(fetchedStreams)
-    }
-
-    fetchStreams()
-  }, [])
-
   return (
     <section>
       <ul>
