c45a01026062c9a0cfd71317a8ad777e0e38f536
wslyvh <wslyvh@users.noreply.github.com>
2022-04-13 12:30:49 +0200

    video player page

---
+++ src/pages/event/[id]/video.tsx
@@ -0,0 +1,58 @@
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { ParsedUrlQuery } from 'querystring'
+import { Event } from 'types'
+import { GetEventNames, GetEvents } from 'services/event'
+import { EmbedLayout } from 'layouts'
+import { SEO } from 'components/seo'
+import Player from 'components/Player'
+import useStreams from 'components/Hooks/useStreams'
+
+interface Props {
+  event: Event
+  events: Event[]
+}
+
+interface Params extends ParsedUrlQuery {
+  id: string
+}
+
+export default function EmbedEventPage(props: Props) {
+  const { streamsLoading, mediaUrl, changeStream } = useStreams(props.event)
+  
+  return <>
+    <SEO title={props.event.name} description={props.event.description} imageUrl={props.event.poster} />
+    <Player src={mediaUrl()} poster={props.event.poster} isLoading={streamsLoading} onStreamError={changeStream} />
+  </>
+}
+
+EmbedEventPage.layout = EmbedLayout
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const events = GetEventNames()
+
+  return {
+    paths: events.map(i => {
+      return { params: { id: i } }
+    }),
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = async context => {
+  const events = GetEvents()
+  const event = events.find(i => i.id === context.params?.id)
+
+  if (!event) {
+    return {
+      props: null,
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      event,
+      events,
+    },
+  }
+}
