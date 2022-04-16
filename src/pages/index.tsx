8a0feffb294a710b02e5f9f9be04234847c295a9
Hans Yadav <hansy@users.noreply.github.com>
2022-04-16 15:56:20 -0400

    check for active streams on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,51 +6,89 @@ import Image from 'next/image'
 import css from './index.module.scss'
 import moment from 'moment'
 import { LivePulse } from 'components/LivePulse'
+import { getStreams } from 'services/stream'
+import { useEffect, useState } from 'react'
 
 interface Props {
   events: Event[]
 }
 
 export function GetDomainName(url: string): string {
-  return url.replace('http://', '').replace('https://', '').replace('www.', '').split(/[\/?#]/)[0]
+  return url
+    .replace('http://', '')
+    .replace('https://', '')
+    .replace('www.', '')
+    .split(/[\/?#]/)[0]
+}
+
+const eventStreamIds = (event: Event) => {
+  return event.rooms.map(room => room.streams.map(stream => stream.id)).flat()
 }
 
 export default function Home(props: Props) {
+  const [activeStreamIds, setActiveStreamIds] = useState<string[]>([])
+
+  useEffect(() => {
+    const getActiveStreams = async () => {
+      // collect all stream ids
+      const streamIds = props.events.map(event => eventStreamIds(event)).flat()
+      const allStreams = await getStreams(streamIds)
+      const activeIds = allStreams.filter(stream => stream.isActive).map(stream => stream.id)
+
+      setActiveStreamIds(activeIds)
+    }
+
+    getActiveStreams()
+  }, [])
+
   const dates = [...new Set(props.events.map(i => i.start).concat(props.events.map(i => i.end)))].sort()
 
   return dates.map(date => {
     const dateHasPassed = moment(date).isBefore(moment())
     const eventDate = new Date(date).getDate()
 
-    return <div key={date} className={css.container}>
-      <h3>{moment(date).format('MMM DD')} {eventDate >= 18 && <small>day {eventDate - 17}</small>}</h3>
+    return (
+      <div key={date} className={css.container}>
+        <h3>
+          {moment(date).format('MMM DD')} {eventDate >= 18 && <small>day {eventDate - 17}</small>}
+        </h3>
 
-      <section>
-        {props.events.filter(event => event.start === date || event.end === date).map((event: Event) => {
-          const eventIsStreaming = false // can we get this at this point?
-          let className = css.event
-          if (dateHasPassed) className += ` ${css.passed}`
+        <section>
+          {props.events
+            .filter(event => event.start === date || event.end === date)
+            .map((event: Event) => {
+              // check if any of the event's stream ids are present within the active stream ids
+              const eventIsStreaming = eventStreamIds(event).some(el => activeStreamIds.includes(el))
 
-          return (
-            <Link key={`event_${event.id}`} href={`event/${event.id}`}>
-              <article className={className}>
-                <div className={css.poster}>
-                  <Image src={event.poster ?? '/posters/default.png'} alt={event.name} objectFit='cover' layout='fill' />
-                </div>
-                <div className={css.info}>
-                  <div className={css.title}>
-                    <h4>{event.name}</h4>
-                    {eventIsStreaming && <LivePulse style={{ marginLeft: '8px'}} />}
-                  </div>                  
-                  <p className={css.description}>{event.description}</p>
-                  <span className={css.domain}>{GetDomainName(event.website)}</span>
-                </div>
-              </article>
-            </Link>
-          )
-        })}
-      </section>
-    </div>
+              let className = css.event
+              if (dateHasPassed) className += ` ${css.passed}`
+
+              return (
+                <Link key={`event_${event.id}`} href={`event/${event.id}`}>
+                  <article className={className}>
+                    <div className={css.poster}>
+                      <Image
+                        src={event.poster ?? '/posters/default.png'}
+                        alt={event.name}
+                        objectFit="cover"
+                        layout="fill"
+                      />
+                    </div>
+                    <div className={css.info}>
+                      <div className={css.title}>
+                        <h4>{event.name}</h4>
+                        {eventIsStreaming && <LivePulse style={{ marginLeft: '8px' }} />}
+                      </div>
+                      <p className={css.description}>{event.description}</p>
+                      <span className={css.domain}>{GetDomainName(event.website)}</span>
+                    </div>
+                  </article>
+                </Link>
+              )
+            })}
+        </section>
+      </div>
+    )
   })
 }
 
