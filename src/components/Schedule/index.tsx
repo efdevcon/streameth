c871c45602cb253d6eac1b55d1371501b6b82cc0
wslyvh <wslyvh@users.noreply.github.com>
2022-04-21 07:48:00 +0200

    update mev, sort schedule

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -9,17 +9,20 @@ export default function Schedule({ sessions }: ScheduleProps) {
   if (sessions.length === 0) {
     return null
   }
+
   return (
     <div>
       <p className="schedule__title">Schedule</p>
       <ul className="schedule__sessions">
-        {sessions.map(session => {
-          return (
-            <li key={`schedule_sessions_${session.id}`}>
-              <ScheduleSession session={session} />
-            </li>
-          )
-        })}
+        {sessions
+          .sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime())
+          .map(session => {
+            return (
+              <li key={`schedule_sessions_${session.id}`}>
+                <ScheduleSession session={session} />
+              </li>
+            )
+          })}
       </ul>
     </div>
   )
