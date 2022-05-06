e1f925bfbc3ed3a22c9214a47177f41d22f647d0
wslyvh <wslyvh@users.noreply.github.com>
2022-05-06 20:52:11 +0200

    increase timeout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 180,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
