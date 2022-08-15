e8e6b80d292e4efccc1f77a620176f7335045821
Hans Yadav <hansy@users.noreply.github.com>
2022-08-14 23:30:28 -0400

    add navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,78 @@
+import { Disclosure } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import Link from 'next/link'
+import Image from 'next/image'
+import css from './Navbar.module.scss'
+import img from 'assets/images/logo.png'
+import Container from 'components/Container'
+
+const rooms = [
+  { name: 'Main', href: '#', current: true },
+  { name: 'Side', href: '#', current: false },
+]
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" className={css.navbar}>
+      {({ open }) => (
+        <>
+          <Container>
+            <div className={css.navbar__inner}>
+              <div className={css.navbar__hamburger}>
+                <Disclosure.Button className={css.navbar__hamburger__button}>
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className={css.navbar__hamburger__button__icon} aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className={css.navbar__hamburger__button__icon} aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className={css.navbar__nav}>
+                <div className={css.navbar__logo}>
+                  <Link href="/">
+                    <a>
+                      <Image src={img} alt="StreamETH" />
+                    </a>
+                  </Link>
+                </div>
+                <div className={css.navbar__nav__items}>
+                  <div className="flex space-x-4">
+                    {rooms.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(item.current ? css.active : '', css.navbar__nav__item)}
+                        aria-current={item.current ? 'page' : undefined}>
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Container>
+
+          <Disclosure.Panel className={css.navbar__menu}>
+            <div className={css.navbar__menu__items}>
+              {rooms.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(item.current ? css.active : '', css.navbar__menu__item)}
+                  aria-current={item.current ? 'page' : undefined}>
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
