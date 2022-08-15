00ed854614bb890c9967065069d19a9bbb968b73
Hans Yadav <hansy@users.noreply.github.com>
2022-08-14 23:45:10 -0400

    apply layout to test event component

--- src/components/EventComponent.tsx
+++ src/components/EventComponent.tsx
@@ -11,21 +11,7 @@ export function TestEventComponent() {
 
   return (
     <div>
-      <br />
-      <section>
-        Active stage: <b>{stage.id}</b>
-      </section>
-      <br />
-
-      <section>
-        <h2 className="text-3xl font-bold underline">{event.name}</h2>
-        <p>
-          {moment(event.start).format('MMM DD')} - {moment(event.end).format('MMM DD')}
-        </p>
-      </section>
-      <br />
-
-      <section>
+      {/* <section>
         <h3 className="text-2xl font-bold">Stages</h3>
         <ul>
           {event.stream.stages.map((i) => {
@@ -36,32 +22,34 @@ export function TestEventComponent() {
             )
           })}
         </ul>
-      </section>
-      <br />
-
+      </section> */}
       <Container>
         <div className={styles.widget}>
-          <div className={styles.header}>Header</div>
+          <div className={styles.header}>
+            <h2 className="text-3xl font-bold underline">{event.name}</h2>
+          </div>
           <div className={styles.player}>Player</div>
-          <div className={styles.sidebar}>Sidebar</div>
-          <div className={styles.eventInfo}>Event Info</div>
+          <div className={styles.sidebar}>
+            <h3 className="text-2xl font-bold">Schedule</h3>
+            <ul>
+              {event.schedule.sessions
+                .sort((a, b) => a.start - b.start)
+                .map((i) => {
+                  return (
+                    <li key={i.id}>
+                      {moment(i.start).format('DD MMM - HH:mm')} {i.name}
+                    </li>
+                  )
+                })}
+            </ul>
+          </div>
+          <div className={styles.eventInfo}>
+            <p>
+              {moment(event.start).format('MMM DD')} - {moment(event.end).format('MMM DD')}
+            </p>
+          </div>
         </div>
       </Container>
-
-      <section>
-        <h3 className="text-2xl font-bold">Schedule</h3>
-        <ul>
-          {event.schedule.sessions
-            .sort((a, b) => a.start - b.start)
-            .map((i) => {
-              return (
-                <li key={i.id}>
-                  {moment(i.start).format('DD MMM - HH:mm')} {i.name}
-                </li>
-              )
-            })}
-        </ul>
-      </section>
     </div>
   )
 }
