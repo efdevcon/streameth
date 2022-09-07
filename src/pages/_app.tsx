e5b47446ec6600f8b1ab1dc8e8b33467c1dd0a1b
wslyvh <wslyvh@users.noreply.github.com>
2022-09-07 13:20:10 +0200

    add matomo plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,10 +14,13 @@ export default function App({ Component, pageProps }: AppLayoutProps) {
   const Layout = Component.layout || ((props) => <DefaultLayout>{props.children}</DefaultLayout>)
 
   useEffect(() => {
-    init({
-      url: 'https://matomo.ethereum.org/',
-      siteId: '34',
-    })
+    const plugin = pageProps.event?.plugins.find((i: any) => i['name'] === 'matomo')
+    if (plugin) {
+      init({
+        url: plugin.config['url'],
+        siteId: plugin.config['siteId'] as string,
+      })
+    }
   }, [])
 
   return (
