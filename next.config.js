162cd0b07f4d95d56a81c6741cc259d0fa25c840
wslyvh <wslyvh@users.noreply.github.com>
2022-09-13 18:06:44 +0200

    single threaded build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   staticPageGenerationTimeout: 180,
+  workerThreads: false,
+  cpus: 1,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
