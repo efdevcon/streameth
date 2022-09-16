d2621265d1f1d52c5033f00f867ebac1a9f9ac97
wslyvh <wslyvh@users.noreply.github.com>
2022-09-16 22:23:45 +0200

    current session by stage

--- src/components/Session/Infobox/index.tsx
+++ src/components/Session/Infobox/index.tsx
@@ -2,7 +2,10 @@ import { Session, Speaker } from 'types'
 import { ShareIcon } from '@heroicons/react/outline'
 import css from './SessionInfoBox.module.scss'
 import SpeakerIconList from 'components/Speaker/IconList'
-import { localizedMoment } from 'utils/dateTime'
+import { currentTimeInUTC, localizedMoment } from 'utils/dateTime'
+import { useStage } from 'hooks/useStage'
+import { useSessions } from 'hooks/useSessions'
+import { useEvent } from 'hooks/useEvent'
 
 interface Props {
   session: Session
@@ -11,20 +14,26 @@ interface Props {
 }
 
 export default function SessionInfoBox({ session, onShareClick, onSpeakerClick }: Props) {
+  const event = useEvent()
+  const stage = useStage()
+  const sessions = useSessions(event)
+  const current = sessions.sessions.find(i => i.stage === stage.id && localizedMoment(i.start).isAfter(currentTimeInUTC()))
+  const currentSession = current ?? session
+
   return (
     <div className={css.box}>
       <div className={css.box__header}>
         <div className={css.box__date}>
-          {localizedMoment(session.start).format('MMM DD / HH:mm')} - {localizedMoment(session.end).format('HH:mm')}
+          {localizedMoment(currentSession.start).format('MMM DD / HH:mm')} - {localizedMoment(currentSession.end).format('HH:mm')}
         </div>
         <ShareIcon className={css.box__shareIcon} onClick={onShareClick} />
       </div>
       <div className={css.box__body}>
-        <p className={css.box__description}>{session.description || session.abstract}</p>
+        <p className={css.box__description}>{currentSession.description || currentSession.abstract}</p>
       </div>
       <div className={css.speakers}>
         <div className={css.speakers__title}>Speakers:</div>
-        <SpeakerIconList speakers={session.speakers} onSpeakerClick={onSpeakerClick} />
+        <SpeakerIconList speakers={currentSession.speakers} onSpeakerClick={onSpeakerClick} />
       </div>
     </div>
   )
