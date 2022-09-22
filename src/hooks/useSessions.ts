5d34059bdb307643cca7de13e3cd1172ef63d11a
Hans Yadav <hansy@users.noreply.github.com>
2022-09-21 20:15:31 -0400

    tweaks

--- src/hooks/useSessions.ts
+++ src/hooks/useSessions.ts
@@ -1,5 +1,5 @@
-import { useState, useEffect, useMemo } from 'react'
-import { Session, Event, Stage } from 'types'
+import { useState, useEffect, useMemo, useCallback } from 'react'
+import { Session, Event } from 'types'
 import { startOfDay, currentTimeInUTC } from 'utils/dateTime'
 import useInterval from '@use-it/interval'
 
@@ -10,31 +10,13 @@ interface Filter {
 
 export type TimeState = 'BEFORE_EVENT' | 'DURING_DAY' | 'BEFORE_NEXT_DAY' | 'AFTER_EVENT'
 
-export function useSessions(event: Event, currentStage: Stage, initFilters: Filter[] = []) {
-  const allSessions = event.schedule.sessions.filter((s) => s.stage === currentStage?.name)
+export function useSessions(event: Event, initFilters: Filter[] = []) {
+  const allSessions = event.schedule.sessions
   const [timeState, setTimeState] = useState<TimeState>('DURING_DAY')
   const [currentSession, setCurrentSession] = useState<Session>(allSessions[0])
   const [eventDayNum, setEventDayNum] = useState<number | null>(null)
   const [filters, setFilters] = useState<Filter[]>(initFilters)
   const eventDays = [...new Set(allSessions.map((i) => startOfDay(i.start)))].sort()
-
-  useEffect(() => {
-    calcTimeState()
-  }, [])
-
-  // determine event day number if during event
-  useEffect(() => {
-    if (eventDays.length > 1) {
-      for (let i = 0; i < eventDays.length; i++) {
-        const day = eventDays[i]
-
-        if (day === startOfDay(currentTimeInUTC())) {
-          setEventDayNum(i + 1)
-          break
-        }
-      }
-    }
-  }, [eventDays])
 
   const sessions = useMemo(() => {
     let filteredSessions = [...allSessions]
@@ -79,37 +61,59 @@ export function useSessions(event: Event, currentStage: Stage, initFilters: Filt
   //   - BEFORE_NEXT_DAY (after all sessions in event day, but before event end)
   //   - AFTER_EVENT (after event end)
   // Also sets currentSession, which determines what session to initially show
-  const calcTimeState = () => {
-    for (let i = 0; i < sessions.length; i++) {
-      const session = sessions[i]
-      const startTime = session.start
-      const endTime = session.end
+  const calcTimeState = useCallback(
+    () => {
+      for (let i = 0; i < sessions.length; i++) {
+        const session = sessions[i]
+        const startTime = session.start
+        const endTime = session.end
+        const currentTime = currentTimeInUTC()
 
-      if (currentTimeInUTC().isBefore(startTime) && i === 0) {
-        setCurrentSession(session)
-        break
-      } else if (currentTimeInUTC().isBefore(endTime)) {
-        setCurrentSession(session)
+        if (currentTime.isBefore(startTime) && i === 0) {
+          setCurrentSession(session)
+          break
+        } else if (currentTime.isBefore(endTime)) {
+          setCurrentSession(session)
 
-        if (startOfDay(currentTimeInUTC()) === startOfDay(endTime)) {
-          setTimeState('DURING_DAY')
-        } else {
-          setTimeState('BEFORE_NEXT_DAY')
+          if (startOfDay(currentTime) === startOfDay(endTime)) {
+            setTimeState('DURING_DAY')
+          } else {
+            setTimeState('BEFORE_NEXT_DAY')
+          }
+          break
+        } else if (i === sessions.length - 1) {
+          setTimeState('AFTER_EVENT')
+          setCurrentSession(sessions[0])
         }
-        break
-      } else if (i === sessions.length - 1) {
-        setTimeState('AFTER_EVENT')
-        setCurrentSession(sessions[0])
+      }
+    },
+    [sessions]
+  )
+
+  useEffect(() => {
+    calcTimeState()
+  }, [calcTimeState])
+
+  // determine event day number if during event
+  useEffect(() => {
+    if (eventDays.length > 1) {
+      for (let i = 0; i < eventDays.length; i++) {
+        const day = eventDays[i]
+
+        if (day === startOfDay(currentTimeInUTC())) {
+          setEventDayNum(i + 1)
+          break
+        }
       }
     }
-  }
+  }, [eventDays])
 
   // update time every minute and determine timeState
   // stop when timeState === BEFORE_NEXT_DAY or AFTER_EVENT
   useInterval(() => {
     calcTimeState()
   },
-    ['BEFORE_NEXT_DAY', 'AFTER_EVENT'].includes(timeState) ? null : 60
+    ['BEFORE_NEXT_DAY', 'AFTER_EVENT'].includes(timeState) ? null : 60000 // time in ms
   )
 
   return {
