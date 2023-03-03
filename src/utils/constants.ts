0571265fcf8adee2c6fcd3dd84423bd530161183
wslyvh <wslyvh@users.noreply.github.com>
2023-03-03 15:18:19 +0100

    update tz

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,5 @@ export const TITLE = 'ETHPorto'
 export const DESCRIPTION = 'Bringing the first ETH event to the beautiful city of Porto.'
 export const SITE_URL = 'https://watch.ethporto.org/'
 export const STREAM_PROVIDER = 'Livepeer'
-export const EVENT_TIMEZONE = 'Europe/Porto'
+export const EVENT_TIMEZONE = 'Europe/Lisbon'
 export const DEFAULT_REVALIDATE_PERIOD = 3600
