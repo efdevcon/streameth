b3145e7b273008e6efe48658fe5bfdefb28b5e2d
wslyvh <wslyvh@users.noreply.github.com>
2023-05-24 11:16:29 +0200

    timezone moment

--- src/utils/dateTime.ts
+++ src/utils/dateTime.ts
@@ -4,14 +4,14 @@ import { EVENT_TIMEZONE } from './constants'
 
 export const startOfDay = (d: moment.Moment | number): number => {
   if (typeof d === 'number') {
-    return moment(d, EVENT_TIMEZONE).startOf('day').valueOf()
+    return moment(d).startOf('day').valueOf()
   }
 
   return d.startOf('day').valueOf()
 }
 
 export const currentTimeInUTC = () => {
-  return moment(EVENT_TIMEZONE) // TODO: Fix properly based on user
+  return moment() // TODO: Fix properly based on user
 }
 
 export const localizedMoment = (value?: number): Moment => {
@@ -19,9 +19,9 @@ export const localizedMoment = (value?: number): Moment => {
 }
 
 export const getDate = (value: number): string => {
-  return moment(value, EVENT_TIMEZONE).format('ddd, MMM D')
+  return moment(value).format('ddd, MMM D')
 }
 
 export const datetimeToUnixTimestamp = (datetime: string): number => {
-  return moment(datetime, EVENT_TIMEZONE).valueOf()
+  return moment(datetime).valueOf()
 }
